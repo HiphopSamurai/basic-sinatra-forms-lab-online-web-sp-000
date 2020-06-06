03f52e772db6843b2f98a030db04fe4997795b9f
Garrett Thompson <gtcruz.gt@gmail.com>
2020-06-06 00:48:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,6 +8,11 @@ class App < Sinatra::Base
   post '/newteam' do
     @team_name = params["name"]
     @couch = params["couch"]
+    @point_guard = params["pg"]
+    @shooting_guard = params["sg"]
+    @small_forward = params["sf"]
+    @power_forward = params["pf"]
+    @center = params["c"]
   end
 
   get '/team' do
